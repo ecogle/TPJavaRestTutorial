2af5670f9f7ff6292ebdf50bc18b4d8efa5b4cdf
Chad Ogle <ecogle@c3devloping.com>
2018-03-10 06:54:25 -0600

    updated the DAO to include some custom people

--- src/com/tutorialspoint/UserDao.java
+++ src/com/tutorialspoint/UserDao.java
@@ -15,13 +15,17 @@ public class UserDao {
 	public List<User> getAllUsers(){ 
 	      
 	      List<User> userList = null; 
-	      String basePath = System.getProperty("user.dir");
+	      
 	      try { 
-	         File file = new File(basePath  + "/src/Users.dat"); 
+	         File file = new File("C:/temp/Users.dat"); 
 	         if (!file.exists()) { 
 	            User user = new User(1, "Mahesh", "Teacher"); 
+	            User user2 = new User(2,"Chad","Ogle");
+	            User user3 = new User(3,"Ashley","McCoy");
 	            userList = new ArrayList<User>(); 
 	            userList.add(user); 
+	            userList.add(user2); 
+	            userList.add(user3); 
 	            saveUserList(userList); 
 	         } 
 	         else{ 
@@ -40,7 +44,7 @@ public class UserDao {
 	   private void saveUserList(List<User> userList){ 
 		   String basePath = System.getProperty("user.dir");
 	      try { 
-	         File file = new File(basePath  + "/src/Users.dat"); 
+	    	  File file = new File("C:/temp/Users.dat"); 
 	         FileOutputStream fos;  
 	         fos = new FileOutputStream(file); 
 	         ObjectOutputStream oos = new ObjectOutputStream(fos); 
